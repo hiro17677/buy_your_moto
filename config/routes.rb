12a97263415e854ef697267b28ac4d6803ecfdf9
Hiro Pan <hiropan@HirodeMacBook-Air.local>
2020-04-20 15:57:50 +0800

    trying heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  resources :privacy_policy
+  resources :contact_ks
+  resources :contact_tc
+  resources :contact_tp
+  resources :buy
+  resources :about
+  resources :welcome
 end
